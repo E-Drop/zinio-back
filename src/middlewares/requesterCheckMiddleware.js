7cbf38ba2bce2cec38c9d8ebce61cff14cfcb892
E-Drop <erosendo.developer@gmail.com>
2022-07-02 15:11:08 +0200

    erase unnecesary console.log

--- src/middlewares/requesterCheckMiddleware.js
+++ src/middlewares/requesterCheckMiddleware.js
@@ -2,7 +2,6 @@ import RequestIp from "@supercharge/request-ip";
 import errorsDispatcher from "../utils/errorsDispatcher.js";
 const requesterCheckMiddleware = (req, res, next) => {
   const ip = RequestIp.getClientIp(req);
-  console.log(ip);
   if(ip !== '::1') {
     return errorsDispatcher(res, "FORBIDDEN");
   }
