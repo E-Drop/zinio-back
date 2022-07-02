fe42ee76bdb826c5bf3f5ebc57b5fe7ff7f7caa5
E-Drop <erosendo.developer@gmail.com>
2022-07-02 15:09:13 +0200

    implement requesterchecker middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import config from 'config';
+
 import issuesRouter from './routes/issuesRouter.js';
+
 import errorsDispatcher from "./utils/errorsDispatcher.js";
 import { constants } from "./utils/constants.js";
+
+import requesterCheckMiddleware from './middlewares/requesterCheckMiddleware.js';
+
+
 const app = express();
 const port = config.get('PORT');
 
 const {ISSUES} = constants.SPECIFIC_URIS;
 
-app.use(ISSUES, issuesRouter());
+app.use(ISSUES, requesterCheckMiddleware, issuesRouter());
 
 
 app.use((req, res) => {
